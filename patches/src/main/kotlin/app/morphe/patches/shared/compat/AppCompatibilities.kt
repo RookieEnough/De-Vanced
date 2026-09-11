3a5251bdc9d3dcdd1ca80434eb494473b8e7b6cc
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-09-11 17:27:12 +0200

    fix(Google Photos): Change to slightly older recommended version for better compatibility

--- patches/src/main/kotlin/app/morphe/patches/shared/compat/AppCompatibilities.kt
+++ patches/src/main/kotlin/app/morphe/patches/shared/compat/AppCompatibilities.kt
@@ -113,7 +113,7 @@ internal object AppCompatibilities {
         packageName = "com.google.android.apps.photos",
         appIconColor = 0xFC3F3C,
         targets = listOf(
-            AppTarget(version = "7.90.0.971743778")
+            AppTarget(version = "7.80.0.929302933")
         )
     )
 
